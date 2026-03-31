8d3007705776e6ea37072ddd3a49f5777d16e3ea
Wlad199 <habilis199@mail.ru>
2026-03-31 17:17:06 +0300

    test

--- app.vue
+++ app.vue
@@ -1,7 +1,34 @@
 <template>
-
+	<div>
+		<p>Текущая дата: {{ formattedDate }}</p>
+		<p>Дата через 5 дней: {{ futureDate }}</p>
+		<p>Время публикации: {{ publicationTime }}</p>
+		<p>Сегодня: {{ $dayjs() }}</p>
+	</div>
+	<div>
+		<NuxtImg src="/public/main.jpg" />
+		<NuxtImg src="/public/01.jpg" />
+	</div>
+	<div>
+		<img src="/public/main.jpg">
+		<img src="/public/01.jpg">
+	</div>
 </template>
 
-<script setup lang='ts'></script>
+<script setup>
+// Получаем доступ к Day.js через $dayjs
+const { $dayjs } = useNuxtApp();
 
-<style scoped lang='scss'></style>
+// Форматируем текущую дату
+const formattedDate = $dayjs().format('DD MMMM YYYY, HH:mm')
+
+// Манипулируем датой
+const futureDate = $dayjs().add(5, 'day').format('YYYY-MM-DD');
+
+// Пример форматирования даты из переменной (например, из API)
+const apiDateString = '2023-10-27T10:00:00Z';
+const publicationTime = $dayjs(apiDateString).format('HH:mm');
+
+// Пример работы с локализацией (если настроена в nuxt.config.ts)
+// const localizedDate = $dayjs().locale('en').format('LL');
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
-	css: ["~/assets/scss/style.scss"]
+	css: ["~/assets/scss/style.scss"],
+	modules: ["dayjs-nuxt", '@nuxt/image'],
+	dayjs: {
+		// Локализация (если нужно)
+		locales: ['ru'], // Список поддерживаемых локалей
+		defaultLocale: 'ru',   // Локаль по умолчанию
+		// Можно также настроить плагины, если они вам нужны (например, для работы с таймзонами)
+		// plugins: ['relativeTime', 'utc', 'advancedFormat'],
+	},
 })
