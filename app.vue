600c2dbead565e620778ba3773b9cf3f32137a60
Wlad199 <habilis199@mail.ru>
2026-04-03 12:22:59 +0300

    add fonts

--- app.vue
+++ app.vue
@@ -1,16 +1,34 @@
 <template>
 	<div>
 		<h2>NuxtImg</h2>
-		<NuxtImg src='/images/main.jpg'/>
+		<NuxtImg src='/images/main.jpg' />
 		<NuxtImg src="/images/01.jpg" />
 	</div>
-	<div>
-		<h2>SRC was corrected</h2>
-		<img src="/public/images/main.jpg">
-		<img src="/public/images/01.jpg">
+	<div class="content">
+		<h2>Title</h2>
+		<p class="en">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur molestiae nulla qui, voluptate pariatur
+			animi nihil aliquam nemo cum! Nostrum, ratione id dolores debitis autem architecto quia sunt a corporis.
+		</p>
+		<p class="rus">
+			лоыфвалмифвыли
+		</p>
 	</div>
 </template>
 
 <script setup>
 
 </script>
+
+<style>
+.content {
+	font-family: "Montserrat";
+}
+
+.en {
+	font-family: Inter;
+	font-weight: 900;
+}
+
+.rus {}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,12 +3,7 @@ export default defineNuxtConfig({
 	compatibilityDate: '2025-07-15',
 	devtools: { enabled: true },
 	css: ["~/assets/scss/style.scss"],
-	modules: ["dayjs-nuxt", '@nuxt/image'],
-	dayjs: {
-		// Локализация (если нужно)
-		locales: ['ru'], // Список поддерживаемых локалей
-		defaultLocale: 'ru',   // Локаль по умолчанию
-		// Можно также настроить плагины, если они вам нужны (например, для работы с таймзонами)
-		// plugins: ['relativeTime', 'utc', 'advancedFormat'],
-	},
-})
+	modules: ["dayjs-nuxt", '@nuxt/image', '@nuxt/fonts'],
+
+}
+)
